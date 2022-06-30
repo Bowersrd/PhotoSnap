508229932fb515a2a3953cb0e25f6cef4667e501
bowersrd <bowersrd@goldmail.etsu.edu>
2022-06-30 10:35:49 -0400

    Added toggle switch component

---
+++ src/components/Pricing/ToggleSwitch.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const isChecked = ref(false);
+
+defineEmits(["updateToggle"]);
+</script>
+
+<template>
+  <label class="flex justify-between items-center">
+    <input
+      v-model="isChecked"
+      type="checkbox"
+      class="appearance-none peer"
+      @change="$emit('updateToggle', isChecked)"
+    />
+    <span
+      class="w-16 h-8 flex items-center flex-shrink-0 p-1 bg-gray rounded-full cursor-pointer transition-transform after:w-6 after:h-6 after:bg-black after:rounded-full peer-checked:after:translate-x-8 after:duration-300"
+    ></span>
+  </label>
+</template>
--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,5 +1,44 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import GridHeadline from "../components/GridHeadline.vue";
+import ToggleSwitch from "../components/Pricing/ToggleSwitch.vue";
+
+const paymentChoice = ref("monthly");
+
+const updatePaymentChoice = (toggled) => {
+  if (toggled) {
+    paymentChoice.value = "yearly";
+  } else {
+    paymentChoice.value = "monthly";
+  }
+};
+</script>
 
 <template>
-  <div></div>
+  <GridHeadline
+    element-tag="header"
+    image="hero.jpg"
+    title="Pricing"
+    body="Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos."
+    is-dark
+    hide-button
+    has-gradient-bar
+  ></GridHeadline>
+  <main class="pt-16">
+    <div class="mb-10 flex justify-center items-center">
+      <p
+        class="text-md mr-8"
+        :class="{ 'font-bold': paymentChoice == 'monthly' }"
+      >
+        Monthly
+      </p>
+      <ToggleSwitch @update-toggle="updatePaymentChoice"></ToggleSwitch>
+      <p
+        class="text-md ml-8"
+        :class="{ 'font-bold': paymentChoice == 'yearly' }"
+      >
+        Yearly
+      </p>
+    </div>
+  </main>
 </template>
