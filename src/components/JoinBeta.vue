b61bcd4373f3c132e2f29b85752f7361abb06842
bowersrd <bowersrd@goldmail.etsu.edu>
2022-06-30 12:45:05 -0400

    Restructured JoinBeta component

--- src/components/JoinBeta.vue
+++ src/components/JoinBeta.vue
@@ -4,18 +4,15 @@ import ArrowButton from "../components/Buttons/ArrowButton.vue";
 
 <template>
   <section class="relative">
-    <div>
-      <img
-        class="w-full"
-        src="@/assets/images/shared/mobile/bg-beta.jpg"
-        alt=""
-      />
-    </div>
-    <div class="absolute top-16 text-white px-7">
-      <h4 class="text-xl font-bold uppercase mb-6">
-        We're in beta. Get your invite today!
-      </h4>
-      <ArrowButton is-inverted>Get an invite</ArrowButton>
+    <div
+      class="w-full bg-[url('./assets/images/shared/mobile/bg-beta.jpg')] bg-cover py-16 px-6"
+    >
+      <div class="text-white">
+        <h4 class="text-xl font-bold uppercase mb-6">
+          We're in beta. Get your invite today!
+        </h4>
+        <ArrowButton is-inverted>Get an invite</ArrowButton>
+      </div>
     </div>
     <div class="absolute top-0 left-7 h-[.375rem] w-1/3 gradient-accent"></div>
   </section>
